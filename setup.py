378a78a1a33f834ae280bf11a11fbddb26dfc4ac
Mayne <i@gine.me>
2020-10-19 18:18:05 +0800

    chore(release): 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vika",
-    version="0.0.1",
+    version="0.0.4",
     author="vikadata",
     author_email="dev@vikadata.com",
     description="维格表官方 Python SDK",
