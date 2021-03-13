72263fb9a670bb21e0b77acafc53118dc7c9a39c
Mayne <i@gine.me>
2021-03-13 15:41:01 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vika",
-    version="0.0.9",
+    version="0.1.0",
     author="vikadata",
     author_email="dev@vikadata.com",
     description="维格表官方 Python SDK",
