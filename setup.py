9035382427a7cc06f3f816248a19291bff606eb5
mayne <i@gine.me>
2021-12-27 16:44:30 +0800

    fix: monotonic_ns => monotonic

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vika",
-    version="1.0.4",
+    version="1.0.5",
     author="vikadata",
     author_email="dev@vikadata.com",
     description="维格表官方 Python SDK",
